daf2f8e70b10098d70f19a75d56e887d3681a6eb
Mathis Antony <sveitser@gmail.com>
2012-12-01 13:42:13 +0800

    wip

--- src/onlinelda.jl
+++ src/onlinelda.jl
@@ -5,14 +5,40 @@ function dirichlet_expecation(alpha)
     psigamma(alpha) - psigamma(sum(alpha))
 end
 
+# this should probably be a class
 function online_lda(vocab, K, D, alpha, eta, tau0, kappa)
 
     lambda = rand(Gamma(100, 1 / 100), K, W)
     Elogbeta = dirichlet_expecation(lambda)
     expElogbeta = exp(Elogbeta)
 
+    function parse_docs(docs)
+        D = size(docs, 1)
+        wordids = 
+    end
+
+    function do_e_step(docs::String)
+        do_e_step([docs])
+    end
+
     function do_e_step(docs)
-        
+        wordids, wordcounts = parse_doc_list(docs, vocab)
+        batchD = size(docs, 1)
+      
+        gamma  = rand(Gamma(100, 1/100, batchD, K))
+        Elogtheta = dirichlet_expecation(gamma)
+        expElogtheta = exp(Elogbeta)
+
+        sstats = zeros(size(lambda))
+        meanchange = 0
+        for d in 1:batchD
+            phinorm = expElogtheta[d, :] * expElogbeta[d, :]' + 1e-100
+            for it in 1:100
+                lastgamma = gamma[d, :]
+                gamma[d, :] = alpha + expElogbeta[d, :] * wordcounts[d] / phinorm  *
+                    expElogbeta[d, :]
+                    exp
+        end
     end
 
     function update_lambda(docs)
